601d79bd12f9fd002498c93c21a6f5c08ecb9424
frknslck <furkanselcuk.fs58@gmail.com>
2023-03-23 23:53:05 +0300

    persistor

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { grey, blueGrey } from "@mui/material/colors";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import { ToastContainer } from "react-toastify";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor } from "./app/store"
 
 function App() {
   const theme = createTheme({
@@ -20,7 +22,9 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <Provider store={store}>
-          <AppRouter />
+          <PersistGate loading={null} persistor={persistor}>
+            <AppRouter />
+          </PersistGate>
         </Provider>
         <ToastContainer />
       </ThemeProvider>
